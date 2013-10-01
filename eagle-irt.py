07ead0554a69931ad39aee68488fad0ede791665
pietrodn <powerpdn@gmail.com>
2013-10-01 22:15:54 +0200

    New version: IRT from XML. Additional info.

--- eagle-irt.py
+++ eagle-irt.py
@@ -1,22 +1,24 @@
 # -*- coding: utf-8 -*-
 
-import pywikibot, csv
+import pywikibot, os, re
+import xml.etree.ElementTree as ET
+
+DATA_DIR = '/Users/pietro/Dropbox/Dati/British School of Rome/'
 
 def main():
 	args = pywikibot.handleArgs()
 	
 	# pywikibot/families/eagle_family.py
 	site = pywikibot.Site('en', 'eagle').data_repository()
-	
-	f = open('EAGLE-IRT.CSV', 'r')
-	r = csv.reader(f, delimiter='	')
-	next(r) # skips first item
-	
 	all = False
 	
-	for row in r:
+	for fileName in os.listdir(DATA_DIR):
+		tree = ET.parse(DATA_DIR + fileName)
+		root = tree.getroot()
+		title = elementText(root.findall('./teiHeader/fileDesc/titleStmt/title')[0])
+	
 		# ID
-		pywikibot.output(">>>>> " + row[0] + " <<<<<")
+		pywikibot.output(">>>>> " + title + " <<<<<")
 		
 		if not all:
 			choice = pywikibot.inputChoice(u"Proceed?",  ['Yes', 'No', 'All'], ['y', 'N', 'a'], 'N')
@@ -26,34 +28,63 @@ def main():
 			all = True
 			choice = 'y'
 		if choice in ['Y', 'y']:
-			page = pywikibot.ItemPage.createNew(site, labels={"en": row[0]}) # New item
+			page = pywikibot.ItemPage.createNew(site, labels={'en': title}) # New item
 			
 			# BSR ID
-			addClaimToItem(site, page, 'P40', row[0])
+			addClaimToItem(site, page, 'P40', fileName[0:-4]) # Remove extension
 			
 			# IPR
-			addClaimToItem(site, page, 'P25', row[2])
+			ipr = elementText(root.findall('./teiHeader/fileDesc/publicationStmt/p')[0])
+			ipr = re.sub(' \(.*?\)', '', ipr)
+			print 'IPR: ' + ipr
+			addClaimToItem(site, page, 'P25', ipr)
 			
 			# EN Translation
 			transClaim = pywikibot.Claim(site, 'P11')
-			transClaim.setTarget(row[4])
+			translationEn = elementText(root.findall('./text/body/div[@type=\'translation\']/p')[0])
+			print 'EN: ' + translationEn
+			transClaim.setTarget(translationEn)
 			page.addClaim(transClaim)
 			
-			periodicalClaim = pywikibot.Claim(site, 'P32')
-			periodicalClaim.setTarget(row[3])
-			
+			# Authors
+			authors = root.findall('./teiHeader/fileDesc/titleStmt/editor')
+			authorString = ''
+			for au in authors:
+				authorString += au.text + ', '
+			authorString = authorString[0:-2]
+			print authorString
 			authorClaim = pywikibot.Claim(site, 'P21')
-			authorClaim.setTarget(row[1])
+			authorClaim.setTarget(authorString)
 			
-			transClaim.addSources([authorClaim, periodicalClaim])
+			# Publication title
+			pubTitle = elementText(root.findall('./teiHeader/fileDesc/sourceDesc//title')[0])
+			print 'PubTitle: ' + pubTitle
+			pubClaim = pywikibot.Claim(site, 'P26')
+			pubClaim.setTarget(pubTitle)
 			
-	f.close()
+			# Publication place
+			pubPlace = elementText(root.findall('./teiHeader/fileDesc/sourceDesc//pubPlace')[0])
+			print 'PubPlace: ' + pubPlace
+			pubPlaceClaim = pywikibot.Claim(site, 'P28')
+			pubPlaceClaim.setTarget(pubPlace)
+			
+			# Date
+			dateText = elementText(root.findall('./teiHeader/fileDesc/sourceDesc//date')[0])
+			print 'Date: ' + dateText
+			dateClaim = pywikibot.Claim(site, 'P29')
+			dateClaim.setTarget(dateText)
+			
+			transClaim.addSources([authorClaim, pubClaim, dateClaim, pubPlaceClaim])
 
 # Adds a claim to an ItemPage.		
 def addClaimToItem(site, page, id, value):
 	claim = pywikibot.Claim(site, id)
 	claim.setTarget(value)
 	page.addClaim(claim)
+
+# Get inner element text, stripping tags of sub-elements.
+def elementText(elem):
+	return ''.join(elem.itertext()).strip()
 
 if __name__ == "__main__":
     try:
